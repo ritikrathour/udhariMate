99df3b0e3b6a55f6c04ff8ca8143351e398f0131
Ritik Rathour <ritikrathour73@gmail.com>
2025-01-08 00:21:36 +0530

    update urls

--- frontend/src/utils/AxiosInstance.js
+++ frontend/src/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: "http://localhost:9000/api/v1",
     timeout: 10000, // Set a timeout (in milliseconds) ,
     withCredentials: true, // Important: This enables cookies to be sent and received
   });
