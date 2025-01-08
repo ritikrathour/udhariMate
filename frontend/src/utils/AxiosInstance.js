80c2f46b30cfe4ea5f66e6f22c6074988e1738e0
Ritik Rathour <99650054+ritikrathour@users.noreply.github.com>
2025-01-08 23:06:24 +0530

    add live url

--- frontend/src/utils/AxiosInstance.js
+++ frontend/src/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: "http://localhost:9000/api/v1",
+    baseURL: "https://uharibackend.onrender.com/api/v1",
     timeout: 10000, // Set a timeout (in milliseconds) ,
     withCredentials: true, // Important: This enables cookies to be sent and received
   });
