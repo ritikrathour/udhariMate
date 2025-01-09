34f4c2fa873d97fa86c5306a559411177236f75c
Ritik Rathour <99650054+ritikrathour@users.noreply.github.com>
2025-01-09 22:35:27 +0530

    Update AxiosInstance.js

--- frontend/src/utils/AxiosInstance.js
+++ frontend/src/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: "https://uharibackend.onrender.com/api/v1",
+    baseURL: "https://udharimate-9fqi.onrender.com/api/v1",
     timeout: 10000, // Set a timeout (in milliseconds) ,
     withCredentials: true, // Important: This enables cookies to be sent and received
   });
